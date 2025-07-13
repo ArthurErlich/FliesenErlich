d99f49897d9e756acf779b9938b2b8ec0273466c
ArthurErlich <a.erlich@live.de>
2025-07-13 17:26:24 +0200

    converted to component

---
+++ components/hero/fliesen-erlich.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="h-[80vh] bg-gray-200 flex justify-center items-center">
+		<div class="pr-[20%]">
+			<h2 class="px-0 mx-0">
+				Fliesen Erlich
+			</h2>
+			<h3 class="px-0 mx-0">
+				Wenn die Qualität stimmen muss!
+			</h3>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" />
+
+<style />
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,6 @@
 <template>
 	<div>
-		<div class="h-[80vh] bg-blue-400 flex justify-center items-center">
-			<div>
-				<h2 class="px-0 mx-0">
-					Erlich
-				</h2>
-				<h3 class="px-0 mx-0">
-					ist Qualität
-				</h3>
-			</div>
-		</div>
+		<HeroFliesenErlich />
 		<h2>Über mich (kurz)</h2>
 		<h2>Aktuelle Arbeit</h2>
 		<h2>Das Bin ich und mein Firma</h2>
