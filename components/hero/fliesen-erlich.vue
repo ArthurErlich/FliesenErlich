08d9a23a7d62ed07bcdebfafde51fc45e10b49c1
ArthurErlich <a.erlich@live.de>
2025-07-18 20:07:35 +0200

    added temp hero image

--- components/hero/fliesen-erlich.vue
+++ components/hero/fliesen-erlich.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="hero-container">
-		<div class="text">
-			<h2 class="px-0 mx-0">
-				Fliesen Erlich
-			</h2>
-			<h3 class="px-0 mx-0">
-				Wenn die Qualität stimmen muss!
-			</h3>
+		<div class="wrapper">
+			<div class="text">
+				<h2 class="px-0 mx-0">
+					Fliesen Erlich
+				</h2>
+				<h3 class="px-0 mx-0">
+					Wenn die Qualität stimmen muss!
+				</h3>
+			</div>
 		</div>
 	</div>
 </template>
@@ -14,17 +16,28 @@
 <script lang="ts" />
 
 <style scoped>
-	.hero-container{
-		padding:0;
-		margin: 0;
-		height: 80vh;
-		background-color: gray;
+.hero-container {
+	padding: 0;
+	margin: 0;
+	height: 80vh;
+	background-color: gray;
+	background-image: url(/hero/15.41.26_ad8dc9dc.jpg);
+	background-size: cover;
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-	.text{
-		padding-right: 20%;
-	}
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+}
+
+.text {
+	border-radius: 2px;
+	background-color: var(--erlich-white);
+	padding-left: 10px;
+	padding-right: 10px;
+}
+
+.wrapper {
+	padding-right: 20%;
+}
 </style>
