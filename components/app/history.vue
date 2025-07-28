b85df3a202dcbc5a4cf979afede1377b16ab000f
haylan <a.erlich@live.de>
2025-07-28 18:05:12 +0200

    history component preperation

---
+++ components/app/history.vue
@@ -0,0 +1,17 @@
+<template>
+	<section>
+		<div />
+	</section>
+</template>
+
+<!-- <script lang="ts" setup>
+Define an array. every array should have the name, date, and descriptiont.
+Styling will create a list or smething to show a hystory of what happend
+export default {
+
+};
+</script> -->
+
+<style>
+
+</style>
