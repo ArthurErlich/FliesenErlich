3e47c338247395bfc78d2d6cbc646041f198c63c
Haylan <a.erlich@live.de>
2025-12-02 22:39:42 +0100

    feat: removed nuxt site

--- server/api/v1/sendMail.php
+++
@@ -1,7 +0,0 @@
-<?php
-//  NOTE! It is better to do it in Node.js, but this is not usable in the webscpace right now
-// check if its a post request
-// check if post reqeust hes correct outhentification
-// check if request comes from correct ip e.g. server ip
-// return html code only
-?>
