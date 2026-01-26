d7b8917f6a46f528fb59b8480cdbd05d6213e896
Arthur Erlich <a.erlich@live.de>
2026-01-26 19:42:27 +0100

    feat:created skeleton plugin

---
+++ repository/AerlFliesenErlichTheme/src/FliesenErlichThemeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aerl\FliesenErlichThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class FliesenErlichThemeBundle extends AbstractBundle {}
