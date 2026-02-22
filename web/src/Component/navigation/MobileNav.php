f9807b6432544ccfec9b1730bbd7e0d390e1ab33
Arthur Erlich <a.erlich@live.de>
2026-02-22 23:18:19 +0100

    feat: started with the navigation interface

---
+++ web/src/Component/navigation/MobileNav.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ErlichFliesen\Component\navigation;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent("Navigation-Mobile", template: 'components/navigation/mobile-nav.html.twig')]
+class MobileNav
+{
+
+}
---
+++ web/src/Component/navigation/TabletNav.php
@@ -0,0 +1,10 @@
+<?php
+namespace ErlichFliesen\Component\navigation;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent("Navigation-Tablet", template: 'components/navigation/tablet-nav.html.twig')]
+class TabletNav
+{
+
+}
