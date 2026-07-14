bee228442bfcbe044643f9fafd7b210187134029
ArthurErlich <a.erlich@live.de>
2026-07-14 11:15:18 +0200

    chore(asro): prepared astro project

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
