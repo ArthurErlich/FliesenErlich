64e90f4b5a9d2cc47e495b81281c0a7412a69342
Arthur Erlich <1+haylan@noreply.localhost>
2026-08-12 17:49:40 +0000

    Infra/staging prod deploy (#21)
    Reviewed-on: https://git.arthurerlich.de/haylan/FliesenErlich/pulls/21

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL,
   integrations: [sitemap(), icon()],
 
   vite: {
